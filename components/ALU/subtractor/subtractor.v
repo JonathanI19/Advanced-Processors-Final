1ff68d1cf9357002b0bd999dcfd6c5d703881ca5
JonathanI19 <jmisely19@gmail.com>
2023-11-01 18:24:41 -0400

    Working subtractor

--- components/ALU/subtractor/subtractor.v
+++ components/ALU/subtractor/subtractor.v
@@ -7,10 +7,10 @@ module subtractor(a, b, dif, ovf);
     output ovf;
     wire c_in = 1'b0;
     
+    // Perform twos complement operation
     wire[63:0] twos_comp = ~b + 1'b1;
     
     // Instantiating sixty_four_bit_adder module
-    // Note that b is inverted and c_in will always be 1
     sixty_four_bit_adder s1(.a(a), .b(twos_comp), .c_in(c_in), .sum(dif), .ovf(ovf));
     
 endmodule   :subtractor
