4d720627c4d2cacc1761b6032cd45e947572dd9c
JonathanI19 <jmisely19@gmail.com>
2023-11-07 15:23:24 -0500

    Created shift left 1 module

---
+++ components/misc/shift_left_1.v
@@ -0,0 +1,13 @@
+// shift left 1 module
+module shift_left_1(in, out);
+
+    // I/O
+    input [63:0]in;
+    output reg [63:0]out;
+
+    always @ *
+        begin
+            out = in << 1;
+        end
+
+endmodule   :shift_left_1
