47b2894eb3488346d81ab4b821ac3a61d1abd9f4
JonathanI19 <jmisely19@gmail.com>
2023-11-09 11:59:50 -0500

    Created test benches for mux and shift

---
+++ components/misc/mux_tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+
+module mux_tb();
+
+    //inputs
+    reg[63:0] a,b;
+    reg sig;
+    
+    //outputs
+    reg[63:0] out;
+    
+    //Instantiate mux
+    mux m1(.in_0(a), .in_1(b), .sig(sig), .out(out));
+    
+    //changing values every 10ns
+    initial
+        begin
+            a = 64'h0000000000000000;
+            b = 64'hffffffffffffffff;
+
+            #10 sig = 0;
+
+            #10 sig = 1;
+            
+            #10 $finish;
+        end
+endmodule
+
---
+++ components/misc/shift_left_1_tb.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+
+module shift_left_1_tb();
+
+    //inputs
+    reg[63:0] in;
+
+    //outputs
+    reg[63:0] out;
+    
+    //Instantiate mux
+    shift_left_1 s1(.in(in), .out(out));
+    
+    //changing values every 10ns
+    initial
+        begin
+            
+            #10 in = 64'b000111111111001;
+
+            #10 sig = 64'b110101010011010;
+            
+            #10 $finish;
+        end
+endmodule
+
